c77cef2cdd03433bafe10437e7d9ba97e746ea8c
BenTech <dipugafrica@gmail.com>
2025-06-06 12:53:35 +0300

    Revert

--- hash.php
+++ hash.php
@@ -6,6 +6,6 @@ $password = 'BenTech@#5428';
 // Create a BCRYPT hash
 $hash = password_hash($password, PASSWORD_BCRYPT);
 
-// Display the result new
+// Display the result 
 echo "Hashed password: " . $hash;
 ?>
